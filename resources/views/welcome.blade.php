1d8931a680d98e108adab378bad8153242cd8062
samandar <xkas2001@gmail.com>
2023-01-07 23:54:47 +0500

    Categories Frontend and Publish error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         </div>
         <div class="flex flex-col items-center mt-12 text-center">
             <span class="relative inline-flex w-full md:w-auto">
-                <a href="{{--{{ route('reservations.step.one') }}--}}" type="button"
+                <a href="{{ route('reservations.step.one') }}" type="button"
                    class="inline-flex items-center justify-center px-6 py-2 text-base font-bold leading-6 text-white bg-green-600 rounded-full lg:w-full md:w-auto hover:bg-green-500 focus:outline-none">
                     Make your Reservation
                 </a>
@@ -122,7 +122,7 @@
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
             <div class="grid lg:grid-cols-4 gap-y-6">
-                {{--@foreach ($specials->menus as $menu)
+                @foreach ($specials->menus as $menu)
                     <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                         <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
                         <div class="px-6 py-4">
@@ -134,7 +134,7 @@
                             <span class="text-xl text-green-600">${{ $menu->price }}</span>
                         </div>
                     </div>
-                @endforeach--}}
+                @endforeach
             </div>
         </div>
     </section>
