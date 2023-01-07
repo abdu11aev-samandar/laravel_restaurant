3452018d69d7f3c8fcb7535f08c184276060b22a
samandar <xkas2001@gmail.com>
2023-01-07 10:25:35 +0500

    Edit and Delete Menu

---
+++ resources/views/admin/menus/edit.blade.php
@@ -0,0 +1,73 @@
+<x-admin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.menus.index') }}"
+                   class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
+                    Menus Index
+                </a>
+            </div>
+
+            <div class="m-2 p-2 bg-slate-100 rounded">
+                <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
+                    <form action="{{ route('admin.menus.update', $menu->id) }}" method="post"
+                          enctype="multipart/form-data">
+                        @csrf
+                        @method('put')
+                        <div class="mb-6">
+                            <label for="name"
+                                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" id="name" name="name" value="{{ $menu->name }}"
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        </div>
+
+                        <div class="mb-6">
+                            <label for="price"
+                                   class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                            <input type="number" id="base-input" name="price" min="0.00" max="1000.00" step="0.01"
+                                   value="{{ $menu->price }}"
+                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        </div>
+
+                        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Image</label>
+                        <div>
+                            <img src="{{ Storage::url($menu->image) }}" class="h-20 w-20 px-4 py-2">
+                        </div>
+                        <input
+                            class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                            aria-describedby="user_avatar_help" id="user_avatar" name="image" type="file">
+
+                        <label for="message" class="block mt-6 mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <textarea id="message" rows="4" name="description"
+                                  class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                  placeholder="Leave a comment...">{{ $menu->description }}</textarea>
+
+                        <label for="message" class="block mt-6 mb-2 text-sm font-medium text-gray-900 dark:text-white">Categories</label>
+                        <select multiple name="categories[]"
+                                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            @foreach($categories as $category)
+                                <option
+                                    value="{{ $category->id }}" @selected($menu->categories->contains($category))>{{ $category->name }}</option>
+                            @endforeach
+                        </select>
+
+                        <div class="mt-6">
+                            <button type="submit"
+                                    class=" px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Update
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</x-admin-layout>
+
