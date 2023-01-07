35d661adbc535673867ea224fcde651065106cb7
samandar <xkas2001@gmail.com>
2023-01-07 21:14:24 +0500

    Frontend Layout and Create Route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
+use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
+use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,6 +25,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/categories',[FrontendCategoryController::class,'index'])->name('categories.index');
+Route::get('/categories/{category}',[FrontendCategoryController::class,'show'])->name('categories.show');
+Route::get('/menus',[FrontendMenuController::class,'index'])->name('menus.index');
+Route::get('/reservation/step-one',[FrontendReservationController::class,'stepOne'])->name('reservations.step.one');
+Route::get('/reservation/step-two',[FrontendReservationController::class,'stepTwo'])->name('reservations.step.two');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -34,11 +42,5 @@ Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(fun
     Route::resource('/tables', TableController::class);
     Route::resource('/reservations', ReservationController::class);
 });
-
-/*Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});*/
 
 require __DIR__ . '/auth.php';
